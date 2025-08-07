b5314831a7305161412ffa9158d9b70d9dc0067d
Afifa Ali <afifaali931@gmail.com>
2025-08-07 16:54:08 +0500

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    id("androidx.navigation.safeargs") version "2.5.1" apply false // For SafeArg
+    id ("org.sonarqube") version "6.0.1.5171" apply false
+}
